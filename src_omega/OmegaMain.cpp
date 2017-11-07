e884a688f5e9d2ddec3a34be3ae12ee4d013620d
Tyler DiBattista <tylerdibo@hotmail.com>
2017-11-07 12:46:34 -0500

    Fixed linux support and started on UUID

--- src_omega/OmegaMain.cpp
+++ src_omega/OmegaMain.cpp
@@ -2,6 +2,11 @@
 #include "Logger.h"
 
 using namespace std;
+#ifdef WIN32
+#include <Rpc.h>
+#else
+#include <uuid/uuid.h>
+#endif
 
 bool checkMACAddress(const char* currMAC){
 	const char* letterMAC = currMAC;
@@ -13,14 +18,15 @@ bool checkMACAddress(const char* currMAC){
 	}
 	
 	const int maxLineLength = 50;
-	char* line = new char[maxLineLength];
+	char* lineArray = new char[maxLineLength];
+	char* lineChar = lineArray;
 	
 	bool done = false;
 	bool foundMatch = false;
 	int lineNumber = 0;
 	while(!done && !foundMatch){
 		lineNumber++;
-		if(!mac_file.getline(line, maxLineLength)){
+		if(!mac_file.getline(lineChar, maxLineLength)){
 			if(mac_file.eof()){
 				done = true;
 			}else{
@@ -29,14 +35,14 @@ bool checkMACAddress(const char* currMAC){
 		}else{
 			letterMAC = currMAC;
 			bool different = false;
-			while(*line != 0 && *letterMAC != 0 && !different){
-				if(*line != *letterMAC){
+			while(*lineChar != 0 && *letterMAC != 0 && !different){
+				if(*lineChar != *letterMAC){
 					different = true;
 				}
-				line++;
+				lineChar++;
 				letterMAC++;
 			}
-			if(*letterMAC != 0 || *line != 0){
+			if(*letterMAC != 0 || *lineChar != 0){
 				different = true;
 			}
 			if(!different){
@@ -45,13 +51,33 @@ bool checkMACAddress(const char* currMAC){
 		}
 	}
 	
-	delete[] line;
+	delete[] lineArray;
 	
 	return foundMatch;
 }
 
+std::string newUUID(){ //credit to: https://stackoverflow.com/questions/543306/platform-independent-guid-generation-in-c
+#ifdef WIN32
+    UUID uuid;
+    UuidCreate(&uuid);
+
+    unsigned char* str;
+    UuidToStringA(&uuid, &str);
+
+    std::string uuidStr((char*)str);
+
+    RpcStringFreeA ( &str );
+#else
+    uuid_t uuid;
+    uuid_generate_random ( uuid );
+    char s[37];
+    uuid_unparse ( uuid, s );
+#endif
+    return uuidStr;
+}
+
 int main(const int argc, const char* const argv[]){
-	Logger logg("test_log.txt", LEVEL_DEBUG); //Initialize log file
+	Logger logg("test_log.txt", LOGGING_LEVEL); //Initialize log file
 	logg.info("Main", "Program Started");
 	
 	if(argc < 2){
@@ -61,11 +87,19 @@ int main(const int argc, const char* const argv[]){
 	if(!checkMACAddress(argv[1])){
 		logg.warning("Main", "MAC address not permitted");
 		//promptForPasscode(); + ask if public computer
+		cout << "Enter passcode to gain access anyways: ";
+		string inputPass;
+		getline(cin, inputPass);
+		//hash the input
+		if(true){ //compare hash to stored passcode
+			
+		}
 	}else{
 		cout << "Match found" << endl;
 	}
 	//logg.info("Main", "MAC address: " + argv[1]);
 	
+	cout << "Enter command: " << endl;
 	
 	//MAIN PROGRAM LOOP
 	bool exit = false;
---
+++ src_omega/Settings.h
@@ -0,0 +1,17 @@
+#ifndef __SETTINGS_H
+#define __SETTINGS_H
+
+
+
+class Settings{
+private:
+	location
+	time?
+	wifinetworks
+	bluetoothdevices?
+	
+public:
+	
+};
+
+#endif /* __SETTINGS_H */
