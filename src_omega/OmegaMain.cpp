8a70cdc10abd924d3b2b3f65fe900f135a7f4799
Daniel Tesfai <danielmtesfai@gmail.com>
2017-11-30 14:14:55 -0500

    I think I fixed the networking errors

--- src_omega/OmegaMain.cpp
+++ src_omega/OmegaMain.cpp
@@ -203,18 +203,19 @@ int networkScan(string SSID){
   wireless_scan_head head;
   iwrange range;
   int sock;
+  char* networkInterface = "wlan0";
 
   /* Open socket to kernel */
   sock = iw_sockets_open();
 
   /* Get some metadata to use for scanning */
-  if (iw_get_range_info(sock, "wlan0", &range) < 0) {
+  if (iw_get_range_info(sock, networkInterface, &range) < 0) {
     logg.error("networkScan", "Error during iw_get_range_info. Aborting.");
     return -1;
   }
 
   /* Perform the scan */
-  if (iw_scan(sock, "wlan0", range.we_version_compiled, &head) < 0) {
+  if (iw_scan(sock, networkInterface, range.we_version_compiled, &head) < 0) {
     logg.error("networkScan", "Error during iw_scan. Aborting.");
     return -1;
   }
@@ -230,8 +231,10 @@ int main(const int argc, const char* const argv[]){
 
 	logg.info("Main", "Program Started");
 
-  if (networkScan < 1){
-    logg.error("Main", "Invalid password or error scanning. Aborting.")
+  string SSID = "eduroam";
+
+  if (networkScan(SSID) < 1){
+    logg.error("Main", "Invalid password or error scanning. Aborting.");
     return -1;
   }
 	
